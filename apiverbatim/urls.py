8219fd4b7bf9a8c688b9dc6c9f8cd78d4021b3bb
Layne Silveira <laynepsilveira@gmail.com>
2023-09-23 16:49:55 -0300

    feature: creating spotify route point

--- apiverbatim/urls.py
+++ apiverbatim/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("transcribe/", include("verbatim.urls")),
+    path("spotify/", include("spotify.urls")),
 ]
