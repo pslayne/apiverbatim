9a9dedc21e5dc4150419704fc4729b7feebd8d59
Layne Silveira <laynepsilveira@gmail.com>
2023-09-28 16:41:28 -0300

    feature: firebase config

---
+++ firebase.py
@@ -0,0 +1,20 @@
+
+import environ
+env = environ.Env()
+environ.Env.read_env('.env')
+
+from firebase_admin import credentials, initialize_app, storage
+
+cred = credentials.Certificate("verbatim-d3d61-firebase-adminsdk-ov20t-a98cc9e9af.json")
+firebase = initialize_app(cred, 
+                          {'storageBucket': 'verbatim-d3d61.appspot.com'})
+bucket = storage.bucket()
+blob = bucket.blob('audio/woman1_wb.wav')
+blob.upload_from_filename('audio/woman1_wb.wav')
+
+# Opt : if you want to make public access from the URL
+blob.make_public()
+
+print("your file url", blob.public_url)
+
+
